318fc2abb5b0d5801382deab29188778e2beb52c
Aurora <auro.kruschewsky@gmail.com>
2023-08-12 13:59:26 -0300

    Project Structure

---
+++ src/caixa-da-lanchonete.js
@@ -0,0 +1,14 @@
+class CaixaDaLanchonete {
+
+    calcularValorDaCompra(metodoDePagamento, itens) {
+        return "";
+        
+        let itens = [cafe, chantily, suco, sanduiche, queijo, salgado, combo1, combo2]
+
+        let precos = [3, 1.5, 6.2, 6.5, 2, 7.25, 9.5, 7.5]
+
+    }
+
+}
+
+export { CaixaDaLanchonete };

172e53c74189da6b0fd5bedc24ebef459a114ce3
Aurora <auro.kruschewsky@gmail.com>
2023-08-12 23:09:29 -0300

    Itens and Quantity Validation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 
-// ADDING INPUTS
+// ADICIONANDO INPUTS
 let i = 1;
 function addInput() {
     
     if (i < 8) {
         i++;
-        console.log(i);
-         // Selecting the Forms
+        console.log("Input " +i+ " Adicionado");
+         // Selecionando a Div
         var itens = document.getElementById("itens");
 
-        // Creating dinamic <input>
+        // Criando <input> dinâmicos
         var br = document.createElement("br")
         br.id="br" + i;
         var inputO = document.createElement("input");
@@ -26,71 +26,153 @@ function addInput() {
         inputQ.max="10";
         inputQ.required = true;
 
-        // Adding to the container element
+        // Adicionando os elementos
         itens.appendChild(br);
         itens.appendChild(inputO);
         itens.appendChild(inputQ);
     } else {
-        alert("Número máximo de itens atingido!!")
+        alert("Número máximo de itens atingido.")
+        console.log("Número máximo de inputs atingidos!")
     }
 
     
 }
 
-// REMOVING INPUTS
+// REMOVENDO INPUTS
 function removeInput() {
     if (i > 1) {
         
-        // Selecting the Inputs
+        // Selecionando os inputs
         var removeBr = document.getElementById("br" + i);
         var removeO = document.getElementById("getPed" + i);
         var removeQ = document.getElementById("getQtd" + i);
 
-        // Removing Inputs
+        // Removendo Inputs
         removeBr.remove();
         removeO.remove();
         removeQ.remove();
+        console.log("Input " +i+ " Removido");
         i--;
-        console.log(i);
     } else {
-        alert("Número mínimo de itens atingido!!")
+        alert("Não é possivel ter menos que 1 item.")
+        console.log("Número mínimo de itens atingido!")
     }
 }
 
 function getInfo() {
 
-    // Getting Payment Method - FEITO
-    let getPag = document.getElementById("getPay").value;
+    // Itens e Preços Pré-Estabelecidos
+    let itensRegistrados = ["cafe", "chantily", "suco", "sanduiche", "queijo", "salgado", "combo1", "combo2"];
+
+    let precos = [3, 1.5, 6.2, 6.5, 2, 7.25, 9.5, 7.5];
+
+    // Recendo Forma de Pagamento
+    let metodoDePagamento = document.getElementById("getPay").value;
     let adjustment;
 
-    // Verification - FEITO?
+    // Verificação - Método de Pagamento
     switch (true) {
-        // Empty Input
-        case (getPag == ""):
-            alert("Por favor, insira uma forma de pagamento!")
-            break;
+        case (metodoDePagamento == ""):
+            alert("Por favor, insira uma forma de pagamento.");
+            console.log("Não foi inserido a forma de pagamento.");
+            return;
 
         // Credito
-        case (getPag == "credito"): 
+        case (metodoDePagamento == "credito"): 
             adjustment = 0.03
-            alert("cred")
+            console.log("Pagamento Crédito.");
             break;
 
         // Debito
-        case (getPag == "debito"): 
+        case (metodoDePagamento == "debito"): 
             adjustment = 0
-            alert("deb")
+            console.log("Pagamendo Débito.");
             break;
 
         // Dinheiro
-        case (getPag == "dinheiro"): 
+        case (metodoDePagamento == "dinheiro"): 
             adjustment = 0.05
-            alert("din")
+            console.log("Pagamento Dinheiro.");
             break;
 
-        // N/A
-        default: alert("Forma de Pagamento Inválida"); break;
+        // Invalido
+        default: alert("Forma de pagamento inválida!"); console.log("Forma de pagamento inválida."); return;
     };
+
+    // Validação de Itens
+    let itens = [];
+    c = 1;
+    while (c <= i) {
+        let codItem = document.getElementById("getPed"+c).value;
+        if (codItem == "" && i == "1") {
+            alert("Não há itens no carrinho de compra!");
+            console.log("Não foram inseridos itens no carrinho.");
+            return;
+        }
+
+        let valItem1 = itensRegistrados.indexOf(codItem);
+        if (valItem1 == "-1") {
+            alert('Item inválido!');
+            console.log('O item "'+codItem+'" inserido no input '+c+' é inválido.');
+            return;
+        }
+        itens.push(codItem);
+        c++;
+    };
+
+    // Validação Item Extra - Chantily
+    let valItem2 = itens.indexOf("chantily");
+    if (valItem2 != "-1") {
+        let valItem2 = itens.indexOf("cafe");
+        if (valItem2 == "-1") {
+            alert("Item extra não pode ser pedido sem o principal");
+            console.log("Não é possivel pedir chantily sem pedir café.");
+            return;
+        }
+    }
+
+    // Validação Item Extra - Chantily
+    let valItem3 = itens.indexOf("queijo");
+    if (valItem3 != "-1") {
+        let valItem3 = itens.indexOf("sanduiche");
+        if (valItem3 == "-1") {
+            alert("Item extra não pode ser pedido sem o principal");
+            console.log("Não é possivel pedir queijo sem pedir sanduiche.");
+            return;
+        }
+    }
+
+    // Validação Preenchimento
+    let valItem4 = itens.indexOf("")
+    if (valItem4 != "-1") {
+        alert("Por favor, preencha todos os campos antes de finalizar a compra.");
+        console.log("Não foram preenchidos todos os inputs disponíveis.")
+        return;
+    }
+
+    console.log("Tamanho do array Itens: "+itens.length);
+    console.log("Itens: "+itens);
+
+    // Validação de Quantidade
+    let quantity = [];
+    c = 1;
+    while (c <= i) {
+        let quantityItem = document.getElementById("getQtd"+c).value;
+        if (quantityItem < 1) {
+            alert("Quantidade inválida!");
+            console.log("Não é possivel pedir 0 itens.")
+            return;
+        }
+        if (quantityItem > 10) {
+            alert("Quantidade inválida!");
+            console.log("Não é possivel pedir mais de 10 itens por produto.");
+            return;
+        }
+        quantity.push(quantityItem);
+        c++;
+    };
+    alert("Length Quantity"+quantity.length);
+    alert(quantity);
 
     // Formatting Itens, Quantity e Array Checkout
     let checkout = [];
@@ -101,20 +183,8 @@ function getInfo() {
         checkout.push(order);
         c++;
     }
-    alert(checkout);
-
-    // let itens = [cafe, chantily, suco, sanduiche, queijo, salgado, combo1, combo2]
-
-    // let precos = [3, 1.5, 6.2, 6.5, 2, 7.25, 9.5, 7.5]
     
 }
 
 
-// let metodoDePagamento = prompt("Escolha o método de pagamento desejado (1-Crédito | 2-Débito | 3-Dinheiro):")
-
-// let itens = ["cafe", "chantily", "suco", "sanduiche", "queijo", "salgado", "combo1", "combo2"];
-
-// let qtd = prompt("Insira a quantidade de cada item:");
-
-// let precos = [3, 1.5, 6.2, 6.5, 2, 7.25, 9.5, 7.5];
 

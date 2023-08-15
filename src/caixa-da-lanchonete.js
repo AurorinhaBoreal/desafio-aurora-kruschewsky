8dd284d3fc735c5cc6ff19d74048445e25c96a93
Aurora <auro.kruschewsky@gmail.com>
2023-08-15 16:14:10 -0300

    Tests Validation 3

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -6,6 +6,7 @@ class CaixaDaLanchonete {
         let precosRegistrados = [3, 1.5, 6.2, 6.5, 2, 7.25, 9.5, 7.5];
         let lenItens = itens.length;
         let separacao;
+        let arrayItens = [];
         let cont = 0;
         let resultado;
         let ajuste;
@@ -40,17 +41,57 @@ class CaixaDaLanchonete {
         }
 
         // Verificação de Itens Válidos e Quantidade Válida
+        let valIdacaoItens;
+        let validacaoQtd;
         while (cont < lenItens) {
             separacao = itens[cont].split(",")
-            let valIdacaoItens = itensRegistrados.indexOf(separacao[0]);
-            let validacaoQtd = separacao[1];
+            valIdacaoItens = itensRegistrados.indexOf(separacao[0]);
             if (valIdacaoItens == "-1") {
                 resultado = "Item inválido!"
                 return resultado;
             }
+            validacaoQtd = separacao[1];
             if (validacaoQtd < "1") {
                 resultado = "Quantidade inválida!"
                 return resultado;
+            }
+            cont++;
+        }
+
+        // Populando arrayItens
+        cont = 0;
+        while (cont < lenItens) {
+            separacao = itens[cont].split(",")
+            arrayItens.push(separacao[0])
+            cont++;
+        }
+
+        // Validação Item Extra - Chantily
+        let validacaoExtraC;
+        cont = 0;
+        while (cont < lenItens) {
+            validacaoExtraC = arrayItens.indexOf("chantily");
+            if (validacaoExtraC != "-1") {
+                validacaoExtraC = arrayItens.indexOf("cafe");
+                if (validacaoExtraC == "-1") {
+                    resultado = "Item extra não pode ser pedido sem o principal"
+                    return resultado;
+                }
+            }
+            cont++;
+        }
+
+        // Validação Item Extra - Queijo
+        let validacaoExtraQ;
+        cont = 0;
+        while (cont < lenItens) {
+            validacaoExtraQ = arrayItens.indexOf("queijo");
+            if (validacaoExtraQ != "-1") {
+                validacaoExtraQ = arrayItens.indexOf("sanduiche");
+                if (validacaoExtraQ == "-1") {
+                    resultado = "Item extra não pode ser pedido sem o principal"
+                    return resultado;
+                }
             }
             cont++;
         }
@@ -73,13 +114,13 @@ class CaixaDaLanchonete {
             valorPedido = pedidoPreco*pedidoQuantidade;
             valorTotal = valorTotal+valorPedido;
             cont++;
-            
         }
 
         valorAjustado = valorTotal*ajuste;
         valorAjustado = valorAjustado.toFixed(2)
         valorFormatado = valorAjustado.split(".");
         resultado = "R$ "+valorFormatado[0]+","+valorFormatado[1];
+        
 
         return resultado;
     }
